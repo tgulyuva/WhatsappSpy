6a1683eb14e01c73ad82bc5d24f358c7760aa8c6
Tarık GÜLYUVA <tarikgulyuva@hotmail.com>
2020-10-10 02:48:46 +0300

    Whatsapp Spy

---
+++ WhatsappSpy/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WhatsappSpy
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.logList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tmrKontrol = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Takip Edilecek Numara";
+            // 
+            // logList
+            // 
+            this.logList.FormattingEnabled = true;
+            this.logList.Location = new System.Drawing.Point(16, 91);
+            this.logList.Name = "logList";
+            this.logList.Size = new System.Drawing.Size(427, 277);
+            this.logList.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(326, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 20);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Takibe Başla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // tmrKontrol
+            // 
+            this.tmrKontrol.Interval = 500;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(103, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(238, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Numarayı 90xxxxxxxxxx formatında giriniz.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 433);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.logList);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Whatsapp Spy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox logList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer tmrKontrol;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WhatsappSpy/Form1.cs
@@ -0,0 +1,114 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WhatsappSpy
+{
+    public partial class Form1 : Form
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        public Form1()
+        {
+           
+
+            InitializeComponent();
+          
+        }
+
+        public bool IsTestElementPresent(By element)
+        {
+            try
+            {
+                driver.FindElement(element);
+                return true;
+                
+            }
+            catch (NoSuchElementException)
+            {
+
+                return false;
+            }
+
+
+
+        }
+
+        public async void button1_Click(object sender, EventArgs e)
+        {
+           
+            await Task.Run(async () => {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    MessageBox.Show("Lütfen Numara Giriniz.");
+                    return;
+                }
+                else {
+                    driver.Navigate().GoToUrl("https://web.whatsapp.com/");
+                    await Task.Delay(10000);
+                    driver.Navigate().GoToUrl("https://api.whatsapp.com/send?phone="+textBox1);
+                    driver.FindElement(By.CssSelector("#action-button")).Click();
+                    await Task.Delay(2000);
+                    driver.FindElement(By.CssSelector("#fallback_block > div > div > a")).Click();
+
+
+                }
+
+            });
+            tmrKontrol.Enabled = true;
+            
+           
+            }
+        string durum = "";
+        public void Kontrol()
+        {
+            try
+            {
+                if (IsTestElementPresent(By.CssSelector("#main > header > div._33QME > div._2ruUq._3xjAz"))) 
+                {
+                    if (IsTestElementPresent(By.CssSelector("#main > header > div._33QME > div._2ruUq._3xjAz > span")))
+                    {
+                        string cevrimici = driver.FindElement(By.CssSelector("main > header > div._33QME > div._2ruUq._3xjAz > span")).Text;
+                        if (cevrimici == "çevrimiçi")
+                        {
+                            if (durum!="çevrimiçi")
+                            {
+                                durum = "çevrimiçi";
+                                logList.Items.Add("Numara çevrimiçi =>" + textBox1 + "=>" + DateTime.Now);
+                            }
+                        }
+                    }
+                   
+                }
+                else
+                {
+                    if (durum!="çevrimdışı")
+                    {
+                        durum = "çevrimdışı";
+                        logList.Items.Add("Numara çevrimdışı =>" + textBox1 + "=>" + DateTime.Now);
+
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                logList.Items.Add(ex.Message);
+            }
+
+
+            }
+        //#main > header > div._33QME > div._2ruUq._3xjAz
+        //#main > header > div._33QME > div._2ruUq._3xjAz > span
+    }
+}
+
